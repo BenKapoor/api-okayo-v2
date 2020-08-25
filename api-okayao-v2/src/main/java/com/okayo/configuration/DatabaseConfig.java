1861bd9f532d9a97c23b2b2d45c942c338ed49cb
BenKapoor <benjaminkapoor@yahoo.fr>
2020-08-25 12:13:29 +0200

    connection db heroku

--- api-okayao-v2/src/main/java/com/okayo/configuration/DatabaseConfig.java
+++ api-okayao-v2/src/main/java/com/okayo/configuration/DatabaseConfig.java
@@ -1,20 +1,27 @@
 package com.okayo.configuration;
 
-import com.zaxxer.hikari.*;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 public class DatabaseConfig {
+	
+	@Value("${spring.secondedatasource.url}")
+	private String dbUrl;
 
-//  @Value("${spring.datasource.url}")
-//  private String dbUrl;
-//
-//  @Bean
-//  public DataSource dataSource() {
-//      HikariConfig config = new HikariConfig();
-//      config.setJdbcUrl(dbUrl);
-//      return new HikariDataSource(config);
-//  }
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl(dbUrl);
+		return new HikariDataSource(config);
+	}
 }
